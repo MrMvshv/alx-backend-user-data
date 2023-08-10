25ad9c9f9bc8f5db5b332cffdfc963ad504d439d
MrMvshv <machariajosphat001@gmail.com>
2023-08-10 16:48:58 +0000

    forbidden, dan and dasteru

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -33,3 +33,13 @@ def unauth() -> str:
       -  401 error
     """
     abort(401)
+
+
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
+def forbid() -> str:
+    """ GET /api/v1/forbidden
+    Return:
+      -  403 error
+    """
+    abort(403)
+
