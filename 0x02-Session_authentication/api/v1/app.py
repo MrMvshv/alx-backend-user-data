818303de13069af8650fbfcd7aeaca77ea983c11
MrMvshv <machariajosphat001@gmail.com>
2023-08-15 10:11:27 +0000

    session .0

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -34,6 +34,8 @@ def before_request():
     else:
         excluded = ['/api/v1/status/', '/api/v1/unauthorized/',
                     '/api/v1/forbidden/']
+        request.current_user = auth.current_user(request)
+
         if auth.require_auth(request.path, excluded):
             if auth.authorization_header(request) is None:
                 abort(401)
