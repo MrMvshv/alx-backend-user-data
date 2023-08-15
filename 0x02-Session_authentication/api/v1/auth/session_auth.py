93abd0d450d70b745473ba2b0d7e7cd7637c4254
MrMvshv <machariajosphat001@gmail.com>
2023-08-15 11:43:36 +0000

    session 2.0

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -64,21 +64,22 @@ class SessionAuth(Auth):
         return user
 
     def destroy_session(self, request=None):
-        """_summary_
+        """ deletes the user session / logout
 
         Args:
-            request (_type_, optional): _description_. Defaults to None.
-
-        Returns:
-            _type_: _description_
+            request: Defaults to None.
         """
+
         if request is None:
             return False
+
         session_cookie = self.session_cookie(request)
         if session_cookie is None:
             return False
+
         user_id = self.user_id_for_session_id(session_cookie)
         if user_id is None:
             return False
+
         del self.user_id_by_session_id[session_cookie]
         return True
