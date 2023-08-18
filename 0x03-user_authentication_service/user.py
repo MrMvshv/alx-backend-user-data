d2ff8bdb43ace9968b4bba2c6a4d1cca44532abc
MrMvshv <machariajosphat001@gmail.com>
2023-08-18 11:43:04 +0000

    user auth service

---
+++ 0x03-user_authentication_service/user.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+SQLAlchemy model named User
+for a database table named users
+using the mapping declaration of SQLAlchemy
+"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String(250), unique=True, index=True, nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250), nullable=True)
+    reset_token = Column(String(250), nullable=True)
